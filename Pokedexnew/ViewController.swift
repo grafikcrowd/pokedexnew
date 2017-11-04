68176d021510f4c12361cead6ad1f11e68bcb01b
John Luke Chica <JediJohn@Johns-iMac.local>
2017-11-04 16:01:17 +0800

    Initial Commit

---
+++ Pokedexnew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedexnew
+//
+//  Created by John Luke Chica on 04/11/2017.
+//  Copyright © 2017 John Luke Chica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
